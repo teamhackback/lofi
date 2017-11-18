a31139b77e24422ae29269fe4d5d200fedcdc1c2
Madox Sesen <madox.sesen@iteratec.de>
2017-11-18 13:20:48 +0100

    Location overview example

--- src/components/LocationOverview.vue
+++ src/components/LocationOverview.vue
@@ -1,15 +1,42 @@
 <template>
-  <div class="hello">
+  <div class="hello" id="locations">
+    <location-card
+      v-for="location in locationResults"
+      v-bind:location="location"
+      v-bind:key="location.id"
+    ></location-card>
   </div>
 </template>
 <script>
-export default {
-  name: 'HelloWorld',
-  data () {
-    return {
+  import Vue from 'vue'
+  import VueMaterial from 'vue-material'
+  import 'vue-material/dist/vue-material.css'
+  import LocationCard from './LocationCard'
+
+  Vue.use(VueMaterial)
+
+  export default {
+
+    components: {LocationCard},
+    name: 'HelloWorld',
+    data () {
+      return {
+        locationResults: [{
+          title: 'San Francisco',
+          text: 'West coast mah dude',
+          picture: './static/sf.jpg'
+        }, {
+          title: 'New York',
+          text: 'East coast represent',
+          picture: './static/ny.jpg'
+        }, {
+          title: 'Los Angeles',
+          text: 'City of Angels',
+          picture: './static/la.jpg'
+        }]
+      }
     }
   }
-}
 </script>
 <style scoped>
 </style>
